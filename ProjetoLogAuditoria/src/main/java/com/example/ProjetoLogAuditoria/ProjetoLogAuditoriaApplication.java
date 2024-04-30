6723ca16b3c232e3e3aaed30a1ee3e4f18b2d18a
Felipe Clarindo Assis de Souza <144058028+felipeClarindo1@users.noreply.github.com>
2024-04-30 16:19:52 -0300

    Add files via upload

---
+++ ProjetoLogAuditoria/src/main/java/com/example/ProjetoLogAuditoria/ProjetoLogAuditoriaApplication.java
@@ -0,0 +1,13 @@
+package com.example.ProjetoLogAuditoria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoLogAuditoriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoLogAuditoriaApplication.class, args);
+	}
+
+}
---
+++ ProjetoLogAuditoria/src/test/java/com/example/ProjetoLogAuditoria/ProjetoLogAuditoriaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ProjetoLogAuditoria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoLogAuditoriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
